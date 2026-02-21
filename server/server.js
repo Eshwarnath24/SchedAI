f41bca0165afe8a5b3a3f8d3c8fb23273f669fb9
Eshwarnath24 <gajulaeshwarnath24@gmail.com>
2026-02-21 17:12:28 +0530

    add base get

--- server/server.js
+++ server/server.js
@@ -29,6 +29,15 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('✅ MongoDB Connected'))
     .catch(err => console.log(err));
 
+
+// Root route
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Backend is running successfully",
+        status: "OK"
+    });
+});
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
